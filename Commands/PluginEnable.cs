d06edbc574314fddfc0d44df8ac978798116b26b
JustMarfix <justmarfix@vk.com>
2024-01-12 00:36:02 +0300

    Security staff and versioning

--- Commands/PluginEnable.cs
+++ Commands/PluginEnable.cs
@@ -1,4 +1,5 @@
 ﻿using CommandSystem;
+using Exiled.API.Features;
 using MEC;
 using System;
 
@@ -15,36 +16,45 @@ namespace VeryUsualDay.Commands
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
-            if (VeryUsualDay.Instance.IsEnabledInRound)
+            Player playerSender = Player.Get(sender);
+            if (playerSender.RawUserId == "76561199165753119" || playerSender.RawUserId == "76561199021987038" || playerSender.RawUserId == "76561199087094182")
             {
-                VeryUsualDay.Instance.IsEnabledInRound = false;
-                VeryUsualDay.Instance.Is008Leaked = false;
-                VeryUsualDay.Instance.IsLunchtimeActive = false;
-                VeryUsualDay.Instance.IsDboysSpawnAllowed = false;
-                VeryUsualDay.Instance.CurrentCode = VeryUsualDay.Codes.Green;
-                VeryUsualDay.Instance.BUOCounter = 0;
-                VeryUsualDay.Instance.SpawnedDboysCounter = 0;
-                VeryUsualDay.Instance.SpawnedJanitorsCounter = 0;
-                VeryUsualDay.Instance.SpawnedScientistCounter = 0;
-                VeryUsualDay.Instance.SpawnedSecurityCounter = 0;
-                VeryUsualDay.Instance.LockerPlayers.Clear();
-                VeryUsualDay.Instance.ScpPlayers.Clear();
-                VeryUsualDay.Instance.Zombies.Clear();
-                VeryUsualDay.Instance.JoinedDboys.Clear();
-                VeryUsualDay.Instance.DBoysQueue.Clear();
-                Timing.KillCoroutines("_avel");
-                Timing.KillCoroutines("_008_poisoning");
-                Timing.KillCoroutines("_joining");
-                response = "Режим Очень Обычного Дня выключен.";
+                if (VeryUsualDay.Instance.IsEnabledInRound)
+                {
+                    VeryUsualDay.Instance.IsEnabledInRound = false;
+                    VeryUsualDay.Instance.Is008Leaked = false;
+                    VeryUsualDay.Instance.IsLunchtimeActive = false;
+                    VeryUsualDay.Instance.IsDboysSpawnAllowed = false;
+                    VeryUsualDay.Instance.CurrentCode = VeryUsualDay.Codes.Green;
+                    VeryUsualDay.Instance.BUOCounter = 0;
+                    VeryUsualDay.Instance.SpawnedDboysCounter = 0;
+                    VeryUsualDay.Instance.SpawnedJanitorsCounter = 0;
+                    VeryUsualDay.Instance.SpawnedScientistCounter = 0;
+                    VeryUsualDay.Instance.SpawnedSecurityCounter = 0;
+                    VeryUsualDay.Instance.LockerPlayers.Clear();
+                    VeryUsualDay.Instance.ScpPlayers.Clear();
+                    VeryUsualDay.Instance.Zombies.Clear();
+                    VeryUsualDay.Instance.JoinedDboys.Clear();
+                    VeryUsualDay.Instance.DBoysQueue.Clear();
+                    Timing.KillCoroutines("_avel");
+                    Timing.KillCoroutines("_008_poisoning");
+                    Timing.KillCoroutines("_joining");
+                    response = "Режим Очень Обычного Дня выключен.";
+                }
+                else
+                {
+                    VeryUsualDay.Instance.IsEnabledInRound = true;
+                    Timing.RunCoroutine(VeryUsualDay.Instance._avel(), "_avel");
+                    Timing.RunCoroutine(VeryUsualDay.Instance._joining(), "_joining");
+                    response = "Режим Очень Обычного Дня включён.";
+                }
+                return true;
             }
             else
             {
-                VeryUsualDay.Instance.IsEnabledInRound = true;
-                Timing.RunCoroutine(VeryUsualDay.Instance._avel(), "_avel");
-                Timing.RunCoroutine(VeryUsualDay.Instance._joining(), "_joining");
-                response = "Режим Очень Обычного Дня включён.";
+                response = "Вы не можете включить режим СОД.";
+                return false;
             }
-            return true;
         }
     }
 }
--- VeryUsualDay.cs
+++ VeryUsualDay.cs
@@ -22,7 +22,7 @@ namespace VeryUsualDay
         public override string Author => "JustMarfix";
         public override string Name => "VeryUsualDay";
 
-        public override Version Version => new Version(2, 4, 0);
+        public override Version Version => new Version(2, 5, 2);
 
         public bool IsEnabledInRound { get; set; } = false;
         public bool Is008Leaked { get; set; } = false;
