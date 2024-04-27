a38ef0037191ae39cff250aa98024aabeeeb6f3f
Mikhail Reznichenko <justmarfix@vk.com>
2024-04-27 18:12:47 +0300

    feat: 244 breach and recontainment

---
+++ Commands/breach244.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using CommandSystem;
+using Exiled.API.Features.Pickups;
+using InventorySystem.Items.Usables.Scp244;
+
+namespace VeryUsualDay.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class breach244 : ICommand
+    {
+        public string Command => "breach244";
+        public string[] Aliases => new string[] { };
+        public string Description => "Вызывает НУС объекта SCP-244-A/B. Только для СОД.";
+        
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!VeryUsualDay.Instance.IsEnabledInRound)
+            {
+                response = "Режим СОД не включён!";
+                return false;
+            }
+            
+            var counter = 0;
+            var pickups = Pickup.List.Where(p => (p.Type == ItemType.SCP244a || p.Type == ItemType.SCP244b) && !p.InUse).ToList();
+            foreach (var pickup in pickups)
+            {
+                pickup.As<Scp244Pickup>().State = Scp244State.Active;
+                counter += 1;
+            }
+            
+            response = $"НУС успешно вызван. Активировано {counter} объекта(ов).";
+            return true;
+        }
+    }
+}
---
+++ Commands/recontain244.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using CommandSystem;
+using Exiled.API.Features.Pickups;
+using InventorySystem.Items.Usables.Scp244;
+
+namespace VeryUsualDay.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class recontain244 : ICommand
+    {
+        public string Command => "recontain244";
+        public string[] Aliases => new string[] { };
+        public string Description => "Вызывает ВОУС объекта SCP-244-A/B. Только для СОД.";
+        
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!VeryUsualDay.Instance.IsEnabledInRound)
+            {
+                response = "Режим СОД не включён!";
+                return false;
+            }
+            
+            var counter = 0;
+            var pickups = Pickup.List.Where(p => (p.Type == ItemType.SCP244a || p.Type == ItemType.SCP244b) && !p.InUse).ToList();
+            foreach (var pickup in pickups)
+            {
+                var type = pickup.Type;
+                var pos = pickup.Position;
+                var rot = pickup.Rotation;
+                pickup.Destroy();
+                Pickup.CreateAndSpawn(type, pos, rot).As<Scp244Pickup>().State = Scp244State.Idle;
+                counter += 1;
+            }
+            
+            response = $"ВОУС успешно вызвано. Деактивировано {counter} объекта(ов).";
+            return true;
+        }
+    }
+}
