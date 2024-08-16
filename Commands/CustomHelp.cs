d3170835f69efcf9d6ae9d4f37128c246ba63170
Mikhail Reznichenko <justmarfix@vk.com>
2024-08-16 21:37:05 +0300

    added custom help

---
+++ Commands/CustomHelp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CommandSystem;
+using Exiled.API.Features;
+
+namespace VeryUsualDay.Commands
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class CustomHelp : ICommand
+    {
+        public string Command => "help";
+        public string[] Aliases => new[] { "hhelp", "help" };
+        public string Description => "Описание команд.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            var player = Player.Get(sender);
+            player.SendConsoleMessage(VeryUsualDay.Instance.Config.CustomHelp, "aqua");
+            response = "";
+            return false;
+        }
+    }
+}
--- Config.cs
+++ Config.cs
@@ -860,5 +860,13 @@ namespace VeryUsualDay
             DamageType.AK,
             DamageType.E11Sr
         };
+
+        public string CustomHelp { get; set; } = @"<color=#FFFFFF>---------------</color><color=#FEA831>FOUNDATION-X</color><color=#FFFFFF>---------------</color>
+  <color=#FEA831>ВСЕ КОМАНДЫ ВВОДЯТСЯ В КОНСОЛЬ НА [`] ИЛИ [~] С ТОЧКОЙ (ПРИМЕР - .help)</color>
+  <color=#FEA831>[help]</color><color=#FFFFFF> - Это сообщение.</color>
+  <color=#FEA831>[classd]</color><color=#FFFFFF> - Встать в очередь на спавн за Испытуемого. Доступно в башне.</color>
+  <color=#FEA831>[checkcode]</color><color=#FFFFFF> - Узнать текущий код.</color>
+  <color=#FEA831>[boom]</color><color=#FFFFFF> - Взорваться. Доступно, если на вас надет пояс шахида.</color>
+  <color=#FFFFFF>---------------</color><color=#FEA831>FOUNDATION-X</color><color=#FFFFFF>---------------</color>";
     }
 }
