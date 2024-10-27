2b5d0008599104bc8f355dc550c6be61e13bef39
Mikhail Reznichenko <justmarfix@vk.com>
2024-10-27 14:43:25 +0300

    add shields and bump tag

---
+++ Commands/AShield.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using CommandSystem;
+using Exiled.API.Features;
+
+namespace VeryUsualDay.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class AShield : ICommand
+    {
+        public string Command => "ashield";
+        public string[] Aliases => null;
+        public string Description => "Использование: ashield id float. Добавляет scp shield игроку.";
+        
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!VeryUsualDay.Instance.IsEnabledInRound)
+            {
+                response = "Режим FX не включён!";
+                return false;
+            }
+
+            var args = arguments.ToArray();
+            if (!Player.TryGet(args[0], out var player))
+            {
+                response = "Не удалось найти игрока.";
+                return false;
+            }
+            
+            player.HumeShield = float.Parse(args[1]);
+            response = "Успешно установлено.";
+            return true;
+        }
+    }
+}
---
+++ Commands/HShield.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using CommandSystem;
+using Exiled.API.Features;
+
+namespace VeryUsualDay.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class HShield : ICommand
+    {
+        public string Command => "hshield";
+        public string[] Aliases => null;
+        public string Description => "Использование: hshield id float. Добавляет human shield игроку.";
+        
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!VeryUsualDay.Instance.IsEnabledInRound)
+            {
+                response = "Режим FX не включён!";
+                return false;
+            }
+
+            var args = arguments.ToArray();
+            if (!Player.TryGet(args[0], out var player))
+            {
+                response = "Не удалось найти игрока.";
+                return false;
+            }
+            
+            player.AddAhp(float.Parse(args[1]), float.Parse(args[1]), 0f, 1f);
+            response = "Успешно добавлено.";
+            return true;
+        }
+    }
+}
