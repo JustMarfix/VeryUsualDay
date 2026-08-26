38ef56f0cd48336bf32b30f1e92f002b4c054b6c
anon <romaskvorcov68@gmail.com>
2026-08-26 21:17:25 +0300

    add commnads & cassie's changing

--- Commands/CheckCodePlayer.cs
+++ Commands/CheckCodePlayer.cs
@@ -9,18 +9,36 @@ namespace VeryUsualDay.Commands
         public string Command => "code";
         public string[] Aliases => new string[] { };
         public string Description => "Показывает текущий код FX.";
-        
-        private const string Str1 = "Статус обеда: активен";
-        private const string Str2 = "Статус обеда: неактивен";
-        
-        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+
+        public bool Execute(
+    ArraySegment<string> arguments,
+    ICommandSender sender,
+    out string response)
         {
             if (!VeryUsualDay.Instance.IsEnabledInRound)
             {
                 response = "Режим FX не включён!";
                 return false;
             }
-            response = $"Текущий код - {VeryUsualDay.Instance.CurrentCode.Description()}. {((int)VeryUsualDay.Instance.CurrentCode < 2 ? (VeryUsualDay.Instance.IsLunchtimeActive ? Str1 : Str2) : "")}";
+
+            string lunchStatus = "";
+
+            if ((int)VeryUsualDay.Instance.CurrentCode < 2)
+            {
+                lunchStatus = VeryUsualDay.Instance.IsLunchtimeActive
+                    ? " Статус обеда: активен."
+                    : " Статус обеда: неактивен.";
+            }
+
+            string teslaStatus = VeryUsualDay.Instance.IsTeslaEnabled
+                ? " Тесла-ворота: включены."
+                : " Тесла-ворота: выключены.";
+
+            response =
+                $"Текущий код - {VeryUsualDay.Instance.CurrentCode.Description()}." +
+                lunchStatus +
+                teslaStatus;
+
             return true;
         }
     }
